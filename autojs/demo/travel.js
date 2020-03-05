9fab8afe84fc87846fe343b408b8418e36f7f009
robert88 <841354162@qq.com>
2020-03-05 10:58:01 +0800

    Update travel.js

--- autojs/demo/travel.js
+++ autojs/demo/travel.js
@@ -1,11 +1,17 @@
 function findDogSpace(){
     try{
-        
+        console.show()
    var reatshop = id("iv_shop").findOne(1000).bounds()
    var reatadd = id("iv_shop").findOne(1000).bounds()
 
    var daoigrad = bounds(0,reatshop.bottom,device.width,reatadd.top).find()
-        console.log(daoigrad)
+   console.log(daoigrad.size())
+        
+        if(daoigrad.empty()){
+            toast("找到啦");
+        }else{
+            toast("没找到╭(╯^╰)╮");
+        }
     }catch(e){
           console.log(e.trace) 
     }
