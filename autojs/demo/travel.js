2011e32b6b65b6e3751da42849500e67849f3c82
robert88 <841354162@qq.com>
2020-03-05 14:13:31 +0800

    Update travel.js

--- autojs/demo/travel.js
+++ autojs/demo/travel.js
@@ -4,7 +4,7 @@ function findDogSpace(){
     var rate = device.width/1080
     var x0 = 65 * rate;
     var s = 35 * rate
-    var y0 = 735 * rate
+    var y0 = 740 * rate
     var w = 210 * rate
     var h = 210 * rate
 
@@ -19,7 +19,7 @@ findDogSpace()
 console.show()
     var left,top,right,buttom;
     for(var k=0;k<12;k++){
-       left=alldogs[k].x,top=alldogs[k].y,right=alldogs[k].x+115,buttom=alldogs[k].y+115;
+       left=alldogs[k].x,top=alldogs[k].y,right=alldogs[k].x+125,buttom=alldogs[k].y+125;
        swipe(left, top, right, buttom,1000)
        var in1 =  boundsInside(left, top, right, buttom).find()
        var con = boundsContains(left, top, right, buttom).find()
