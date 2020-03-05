5d131364315dcb0178a94daea2252c654903bb12
robert88 <841354162@qq.com>
2020-03-05 16:21:23 +0800

    Update travel.js

--- autojs/demo/travel.js
+++ autojs/demo/travel.js
@@ -1,22 +1,30 @@
 
+console.show()
+//全部dog
+var rate = device.width/1080
+var s = 35 * rate
+var w = 210 * rate
+var h = 210 * rate
+
 function findDogSpace(){
     var alldogs = []
-    var rate = device.width/1080
     var x0 = 65 * rate;
-    var s = 35 * rate
     var y0 = 740 * rate
-    var w = 210 * rate
-    var h = 210 * rate
 
     for(var j=0;j<4;j++){
         for(var i=0;i<4;i++){
             alldogs.push({x:w/2 + x0 +(s+w)*i,y:h/2 + y0 +(s+h)*j})
         }
     }
-    return alldogs()
+    return alldogs
 }
 
 function getDogInfo(dog){
+
+    var left = dog.x;
+    var top = dog.y;
+    var right = dog.x+(s+w)/2
+    var right = dog.y+(s+h)/2
    var hasDog = boundsInside(left, top, right, buttom).find()
    if(hasDog.size()==1){
     return {level:hasDog.get(0).text(),x:dog.x,y:dog.y}
@@ -37,7 +45,7 @@ function swiperDog() {
             dogs[dog.level].push(dog);
         }
     })
-    console.log(dogs)
+
     swiperingDog( dogs);
 }
 //是否有两只相同的狗
