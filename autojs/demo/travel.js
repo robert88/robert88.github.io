b85ed3222bc449fd043d67abd8dd11d988bfcf83
robert88 <841354162@qq.com>
2020-03-05 17:51:56 +0800

    Update travel.js

--- autojs/demo/travel.js
+++ autojs/demo/travel.js
@@ -1,70 +1,23 @@
 
-console.show()
-//全部dog
-var rate = device.width/1080
-var s = 35 * rate
-var w = 210 * rate
-var h = 210 * rate
+//领取金币
+var topGlodtimer
 
-function findDogSpace(){
-    var alldogs = []
-    var x0 = 65 * rate;
-    var y0 = 740 * rate
+function topGlod() {
 
-    for(var j=0;j<4;j++){
-        for(var i=0;i<4;i++){
-            alldogs.push({x:w/2 + x0 +(s+w)*i,y:h/2 + y0 +(s+h)*j})
-        }
-    }
-    return alldogs
-}
+  var countdonw = id("countdown_view").findOne(1000)
 
-function getDogInfo(dog){
-
-    var left = dog.x;
-    var top = dog.y;
-    var right = dog.x+(s+w)/2
-    var buttom = dog.y+(s+h)/2
-   var hasDog = boundsInside(left, top, right, buttom).find()
-   if(hasDog.size()==1){
-    return {level:hasDog.get(0).text(),x:dog.x,y:dog.y}
-   }else{
-    return null
-   }
-}
-
-
-
-function swiperDog() {
-    var dogspace = findDogSpace();
-    var dogs = {}
-    dogspace.forEach(function(dog){
-        dog = getDogInfo(dog);
-        if(dog){
-            console.log("找到"+dog.level)
-            if(dogs[dog.level]){
-                 console.log("合并"+dog.level)
-                mergeDog(dogs, dog)
-            }else{
-                dogs[dog.level] = Object.assign({},dog)
-            }
-        }
-    })
-}
-
-//是否有两只相同的狗
-function mergeDog( dogs,dog) {
-    var mgdog = dogs[dog.level];
-    swipe(mgdog.x,mgdog.y,dog.x,dog.y,1000);
+  if (countdonw) {
+    id("lyt_free_coin").findOne(1000).click()
     sleep(1000)
-    dogs[dog.level] = null;
-    var addLevel = Math.floor(parseInt(dog.level,10)+1);
-    if(dogs[addLevel]){
-         console.log("递归和并"+addLevel)
-        mergeDog(dogs, {level:addLevel,x:dog.x,y:dog.y})
-    }else{
-        dogs[addLevel] ={level:addLevel,x:dog.x,y:dog.y}
-    }
+    id("iv_close").findOne(1000).click()
+    sleep(1000)
   }
 
-swiperDog();
+  var nextTime = 35 * 60 * 1000;
+
+  clearTimeout(topGlodtimer)
+  //35分钟领取一次
+  topGlodtimer = setTimeout(function() {
+    topGlod();
+  }, nextTime)
+}
