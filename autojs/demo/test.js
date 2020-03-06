9b35ab2bd7b630fde8c45649c12248d12626541e
robert88 <841354162@qq.com>
2020-03-06 08:34:24 +0800

    Update test.js

--- autojs/demo/test.js
+++ autojs/demo/test.js
@@ -1,28 +1,30 @@
+
+setScreenMetrics(1080, 1920);
+// setInterval(() => {
+//     var button = id("button2").findOne(500)
+//     if (button) {
+//         toast("取消前往")
+//         button.click()
+//     }
+// }, 1000);
+
+//全部dog
 var rate = device.width / 1080
-var ratey = device.height / 1920
+
 var s = 35 * rate
-var sy = 35 * ratey
 var w = 210 * rate
-var h = 210 * ratey
+var h = 210 * rate
+
 
 function findDogSpace() {
     var alldogs = []
     var x0 = 65 * rate;
-    var y0 = 740 * ratey
+    var y0 = 740 * rate+(device.height-device.width*1920/1080)
 
     for (var j = 0; j < 3; j++) {
         for (var i = 0; i < 4; i++) {
-            alldogs.push({ x: w / 2 + x0 + (s + w) * i, y: h / 2 + y0 + (sy + h) * j })
+            alldogs.push({ x: w / 2 + x0 + (s + w) * i, y: h / 2 + y0 + (s + h) * j })
         }
     }
     return alldogs
 }
-console.show()
-console.log(device.width)
-console.log(device.height)
-
-findDogSpace().forEach(function (dog) {
-    swipe(dog.x, dog.y, dog.x + (s + w) / 2,
-        dog.y + (s + h) / 2, 1000)
-    sleep(2000)
-})
