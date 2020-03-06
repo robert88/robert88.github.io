6b97ca0b57ab1158c5c3f2ad46a87aaeb8992034
robert88 <841354162@qq.com>
2020-03-06 11:02:46 +0800

    Update require.js

--- autojs/demo/require.js
+++ autojs/demo/require.js
@@ -4,12 +4,18 @@ var time = new Date.getTime();
 function r(name){
     http.get("https://robert88.github.io//autojs/demo/"+name+".js?ver="+time, {}, function(res, err){
         if(err){
+            sleep(1000)
             toast("更新失败")
             return;
         }
         var content = res.body.string();
         
         if(files.exists("./"+name+".js")){
+            if(files.read("./"+name+".js")==content){
+                sleep(1000)
+                return  toast("服务器代码未变动"+"./"+name+".js")
+            }
+            
            files.remove("./"+name+".js")
         }
         files.write("./"+name+".js", content);
