0b14f6bb48f05014e37cd9e75ef4b919f1610ffb
robert88 <841354162@qq.com>
2020-03-06 11:28:52 +0800

    Update require.js

--- autojs/demo/require.js
+++ autojs/demo/require.js
@@ -1,27 +1,42 @@
 //更新对象
 var time = new Date().getTime();
+var ajaxcount = 0;
+var backMessage = [];
 
-function r(name){
-    http.get("https://robert88.github.io//autojs/demo/"+name+".js?ver="+time, {}, function(res, err){
-        if(err){
-            sleep(1000)
-            toast("更新失败")
-            return;
-        }
-        var content = res.body.string();
-        
-        if(files.exists("./"+name+".js")){
-            if(files.read("./"+name+".js")==content){
-                sleep(1000)
-                return  toast("服务器代码未变动"+"./"+name+".js")
-            }
-            
-           files.remove("./"+name+".js")
-        }
-        files.write("./"+name+".js", content);
-        toast("更新成功"+"./"+name+".js")
-        sleep(1000)
-    });
+/**ajax请求 */
+function r(name) {
+  ajaxcount++;
+  http.get("https://robert88.github.io//autojs/demo/" + name + ".js?ver=" + time, {}, function(res, err) {
+    if (err) {
+      t("更新失败")
+      return;
+    }
+    var content = res.body.string();
+
+    if (files.exists("./" + name + ".js")) {
+      if (files.read("./" + name + ".js") == content) {
+        return t("服务器代码未变动" + "./" + name + ".js")
+      }
+
+      files.remove("./" + name + ".js")
+    }
+    files.write("./" + name + ".js", content);
+    t("更新成功" + "./" + name + ".js")
+  });
+}
+
+/* 消息队列*/
+
+function t(message) {
+  ajaxcount--;
+  backMessage.push(message);
+  if (ajaxcount == 0) {
+    backMessage.forEach(function(msg) {
+      toast(msg)
+      sleep(1000)
+    })
+
+  }
 }
 
 r("travel")
