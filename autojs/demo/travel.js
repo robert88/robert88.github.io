23af7df123f11126c74934147dd8832868aeefff
robert88 <841354162@qq.com>
2020-03-07 06:08:05 +0800

    r

--- autojs/demo/travel.js
+++ autojs/demo/travel.js
@@ -1,18 +1,25 @@
-console.show()
-var merge = require("./merge.js");
+console.show();
+
+var merge = require("./travel/merge.js");
 console.log("成功加载组件 merge")
-var gold = require("./gold.js");
+
+var gold = require("./travel/gold.js");
 console.log("成功加载组件 glod")
-var offline = require("./offline.js");
+
+var offline = require("./travel/offline.js");
 console.log("成功加载组件 offline")
-var launchApp = require("./launch.js");
+
+var launchApp = require("./travel/launch.js");
 console.log("成功加载组件 launch")
 
 launchApp("com.jiayouya.travel");
 console.info("-----launchApp -end --")
+
 offline(function(){console.log("endoffline")});
 console.info("-----offline -end --")
+
 gold();
 console.info("-----glod -end --")
+
 merge();
 console.info("-----merge -end --")
