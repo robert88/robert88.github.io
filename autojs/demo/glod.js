3d861157ca56c7285d7d92d0d027595abddf513b
robert88 <841354162@qq.com>
2020-03-06 16:01:38 +0800

    Create glod.js

---
+++ autojs/demo/glod.js
@@ -0,0 +1,27 @@
+//领取金币
+var topGlodtimer=0
+
+function topGlod() {
+
+  var countdonw = id("countdown_view").findOne(1000)
+
+  if (!countdonw) {
+    console.log("点击领金币")
+    id("lyt_free_coin").findOne(1000).click()
+    sleep(1000)
+    console.log("确认领取金币")
+    id("btn").findOne(1000).click()
+    sleep(1000)
+  }else{
+    console.log("还在计时")
+  }
+
+  var nextTime = 600000;
+
+  clearTimeout(topGlodtimer)
+  //35分钟领取一次
+ console.log("十分钟之后再来检查是否可以领取金币")
+  topGlodtimer = setTimeout(function() {
+    topGlod();
+  }, nextTime)
+}
