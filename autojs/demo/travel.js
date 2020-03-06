2da5087701f45544f7b033faa1149a1a539d10eb
robert88 <841354162@qq.com>
2020-03-06 17:46:38 +0800

    Update travel.js

--- autojs/demo/travel.js
+++ autojs/demo/travel.js
@@ -1,10 +1,18 @@
 console.show()
-const merge = require("./merge");
-const glod = require("./glod");
-const offline = require("./offline");
-const launchApp = require("./launch");
-console.log("已经加载全部组件")
+var merge = require("./merge.js");
+console.log("加载组件 merge")
+var glod = require("./glod.js");
+console.log("加载组件 glod")
+var offline = require("./offline.js");
+console.log("加载组件 offline")
+var launchApp = require("./launch.js");
+console.log("加载组件 launch")
+
 launchApp();
+console.info("-----launchApp -end --")
 offline();
+console.info("-----offline -end --")
 glod();
+console.info("-----glod -end --")
 merge();
+console.info("-----merge -end --")
