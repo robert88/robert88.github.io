b1a7d14365f8f21458dc94e5e4edc7bb3714331a
robert88 <841354162@qq.com>
2020-03-06 15:34:30 +0800

    Update travelmerge.js

--- autojs/demo/travelmerge.js
+++ autojs/demo/travelmerge.js
@@ -148,31 +148,23 @@ var lookADTime = 0
 function checkAdEND(flow) {
   console.log("检查广告是否结束")
   //有钱花
-  var ad1 = id("tt_click_upper_non_content_layout").findOne(150000)
-  if (ad1) {
+  var ad2 = id("tt_reward_ad_countdown").findOne(5000)
+  if (ad2) {
     console.log("广告进行中")
     clearTimeout(lookADTime)
     lookADTime = setTimeout(function() {
       checkAdEND(flow)
-    }, 5000)
+    }, 3000)
   } else {
-    var close = id("tt_video_ad_close_layout").findOne(150000)
+     sleep(5000)
+    var close = id("tt_video_ad_close_layout").findOne(5000)
     if (!close) {
       console.error("广告结束但是没有找到结束按钮")
       flow(false);
     } else {
       console.log("广告结束")
       close.click()
-      sleep(5000)
-      var btn = id("btn").findOne(1000);
-      if (!btn) {
-        console.error("观看之后确认收益按钮没找到");
-        flow(false);
-        return;
-      }
-      console.log("点击确认收益按钮");
-      btn.click()
-      sleep(2000)
+      sleep(1000)
       flow(true);
     }
   }
@@ -196,7 +188,21 @@ function lookAD(flow) {
 
   sleep(5000)
 
-  checkAdEND(flow);
+  checkAdEND(function(flag){
+    if(flag){
+      var btn = id("btn").findOne(1000);
+      if (!btn) {
+        console.error("观看之后确认收益按钮没找到");
+        flow(false);
+        return;
+      }
+      console.log("点击确认收益按钮");
+      btn.click()
+      sleep(2000)
+    }else{
+      flow(flag)
+    }
+  });
 
 
 }
