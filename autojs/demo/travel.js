1fcc7fcf15f7141e0099e8895e6eabbb736a6b0d
robert88 <841354162@qq.com>
2020-03-06 17:52:39 +0800

    Update travel.js

--- autojs/demo/travel.js
+++ autojs/demo/travel.js
@@ -1,12 +1,12 @@
 console.show()
 var merge = require("./merge.js");
-console.log("加载组件 merge")
+console.log("成功加载组件 merge")
 var gold = require("./gold.js");
-console.log("加载组件 glod")
+console.log("成功加载组件 glod")
 var offline = require("./offline.js");
-console.log("加载组件 offline")
+console.log("成功加载组件 offline")
 var launchApp = require("./launch.js");
-console.log("加载组件 launch")
+console.log("成功加载组件 launch")
 
 launchApp();
 console.info("-----launchApp -end --")
