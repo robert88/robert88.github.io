8339ea89807cac441f4481d8f7cd3516cc1e9adf
robert88 <841354162@qq.com>
2020-03-06 10:56:04 +0800

    Update require.js

--- autojs/demo/require.js
+++ autojs/demo/require.js
@@ -1,10 +1,21 @@
 //更新对象
 var time = new Date.getTime();
-http.get("https://robert88.github.io//autojs/demo/travel.js?ver="+time, {}, function(res, err){
-    if(err){
-        toast("更新失败")
-        return;
-    }
-    var content = res.body.string();
-    files.write("./travel.js", content);
-});
+
+function r(name){
+    http.get("https://robert88.github.io//autojs/demo/"+name+".js?ver="+time, {}, function(res, err){
+        if(err){
+            toast("更新失败")
+            return;
+        }
+        var content = res.body.string();
+        
+        if(files.exists("./"+name+".js")){
+           files.remove("./"+name+".js")
+        }
+        files.write("./"+name+".js", content);
+    });
+}
+
+r("travel")
+r("travelmerge")
+r("test")
