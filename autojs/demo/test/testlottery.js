06ba79ded49520fb27ea1b733a7652b78f6e12b1
robert88 <841354162@qq.com>
2020-03-06 15:41:25 +0800

    Create testlottery.js

---
+++ autojs/demo/test/testlottery.js
@@ -0,0 +1,22 @@
+//领取金币
+var topGlodtimer
+
+function topGlod() {
+
+  var countdonw = id("countdown_view").findOne(1000)
+
+  if (!countdonw) {
+    id("lyt_free_coin").findOne(1000).click()
+    sleep(1000)
+    id("iv_close").findOne(1000).click()
+    sleep(1000)
+  }
+
+  var nextTime = 35 * 60 * 1000;
+
+  clearTimeout(topGlodtimer)
+  //35分钟领取一次
+  topGlodtimer = setTimeout(function() {
+    topGlod();
+  }, nextTime)
+}
