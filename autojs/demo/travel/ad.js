b5660650991c25a32517c04d39d17eb985a035a0
robert88 <841354162@qq.com>
2020-03-09 16:44:17 +0800

    Update ad.js

--- autojs/demo/travel/ad.js
+++ autojs/demo/travel/ad.js
@@ -3,37 +3,49 @@
 var lookADTime = 0
 
 function checkAdEND(flow) {
+  
+ console.log("启动关闭声音")
+  
+ closeVolume();
+  
   console.log("检查广告是否结束")
   //有钱花
-  var ad2 = id("tt_reward_ad_countdown").findOne(5000)
-  if (ad2) {
+  var close = id("tt_video_ad_close_layout").findOne(5000)
+  if (!close) {
     console.log("广告进行中")
     clearTimeout(lookADTime)
     lookADTime = setTimeout(function() {
       checkAdEND(flow)
     }, 3000)
   } else {
-    sleep(5000)
-    var close = id("tt_video_ad_close_layout").findOne(5000)
-    if (!close) {
-      console.error("广告结束但是没有找到结束按钮")
-      flow(false);
-    } else {
       console.log("广告结束")
       close.click()
       sleep(1000)
       flow(true);
-    }
   }
+}
+
+/*关闭声音*/
+var maxCount=0;
+function closeVolume(){
+ maxCount++;
+  if(maxCount<10){
+    var currentVolume =  device.getMusicVolume;
+    VolumeDown();
+    if(currentVolume){
+       closeVolume()
+    }else{
+      console.log("声音已关闭");
+    }
+  }else{
+      console.log("声音关闭不了");
+  }
+  maxCount--
 }
 
 /**观看广告*/
 function lookAD(flow) {
-  var currentVolume = device.getMusicVolume();
-  if (currentVolume) {
-    alert("播放广告有声音")
-    return;
-  }
+  
   var btn = id("btn_see").findOne(1000)
   if (!btn) {
     console.error("没有找到观看视频的按钮");
@@ -42,7 +54,6 @@ function lookAD(flow) {
   }
   console.log("进入广告")
   btn.click();
-
   sleep(5000)
 
   checkAdEND(function(flag) {
