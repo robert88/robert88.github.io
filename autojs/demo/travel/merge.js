1e979d7e8b585af79c804ff82c2de908b869203f
robert88 <841354162@qq.com>
2020-03-10 09:19:17 +0800

    Update merge.js

--- autojs/demo/travel/merge.js
+++ autojs/demo/travel/merge.js
@@ -1,5 +1,64 @@
-
 var lookAd = require("./ad.js");
+
+var gold = require("./gold.js");
+console.log("成功加载组件 glod")
+
+var offline = require("./offline.js");
+console.log("成功加载组件 offline")
+
+var launchApp = require("./launch.js");
+console.log("成功加载组件 launch")
+
+var city = require("./city.js");
+console.log("成功加载组件 city")
+
+var appName = "com.jiayouya.travel";
+var friendName = "rap";
+var tryTime=0
+
+
+ function handlerError(msg){
+
+    console.error(msg);
+
+    if(tryTime>15){
+        error.apply("已经尝试重启15次");
+        return;
+    }
+    console.log("唤醒屏幕")
+    device.wakeUpIfNeeded();
+    //检查app是否还在
+    if (currentPackage() != app) {
+        console.log("当前不在app内，服务重新启动");
+        run();
+    }else{
+        var close = id("iv_close").findOne(3000);
+        var close2 = id("tt_video_ad_close_layout").findOne(5000)
+        var btn = id("btn").findOne(1000);
+        var button2 = id("button2").findOne(1000);
+
+        if(close){
+            console.log("发现未知关闭按钮,服务重新启动");
+            close.click();
+            run();
+        }else if(close2){
+            console.log("发现未知广告关闭按钮,服务重新启动");
+            close2.click();
+            run();
+        }else if(btn){
+            console.log("发现未知确认按钮,服务重新启动");
+            btn.click();
+            run();
+        }else if(button2){
+            console.log("发现未知系统取消按钮,服务重新启动");
+            button2.click();
+            run();
+        }
+    }
+  
+}
+
+app.e.on("systemError",handlerError)
 
 //坐标参数
 var rate = device.width / 1080
@@ -14,7 +73,7 @@ function findDogSpace() {
   var fristOne = id("tv").className("android.widget.TextView").text("旅行").findOne(3000);
   if(!hashome){
     if(!fristOne){
-      console.error("未知页面");
+      throw Error("未知页面");
       return;
     }else{
       hashome = device.height - fristOne.bounds().bottom;
@@ -129,7 +188,7 @@ function checkneedlookAd(flow) {
     //升级界面，等等
     var btn = id("btn").findOne(1000);
     if (!btn) {
-      console.error("未知界面")
+      throw Error("未知界面")
       return;
     } else {
       btn.click();
@@ -187,9 +246,33 @@ function initflow() {
 
  if(canbuyTimes){
   app.g(initflow,null,0)
+ }else{
+  app.g(gold,null,60000)
+  app.g(city,null,0)
  }
 
 
 }
 
-module.exports = initflow
+
+function run(){
+
+  console.log("保持手机常亮")
+
+  device.keepScreenOn();
+
+  app.g(launchApp,appName,0)
+  
+  app.g(offline,friendName,0)
+
+  app.g(gold,null,0)
+  
+  app.g(city,null,0)
+  
+  app.g(initflow,null,0);
+
+}
+
+
+run();
+
