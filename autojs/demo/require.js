f1a060b36bffffb1a6bbb8618ec4872f7f31e7e9
robert88 <841354162@qq.com>
2020-03-10 09:49:20 +0800

    Update require.js

--- autojs/demo/require.js
+++ autojs/demo/require.js
@@ -1,3 +1,4 @@
+console.show()
 //更新对象
 var time = new Date().getTime();
 var ajaxcount = 0;
@@ -5,11 +6,13 @@ var ajaxcount = 0;
 
 /**ajax请求 */
 function r(name,localname) {
+  console.log("请求",name)
   localname = localname ||name
   ajaxcount++;
   http.get("https://robert88.github.io//autojs/demo/" + name + ".js?ver=" + time, {},
    function(res, err) {
     ajaxcount--;
+     console.log("请求end",name,ajaxcount)
     c(name,localname,res, err);
   });
 }
@@ -41,12 +44,16 @@ function w(name,localname,res, err){
   var content = res.body.string();
 
   if (files.exists("./" + localname + ".js")) {
+      console.log("存在",localname)
     if (files.read("./" + localname + ".js") == content) {
       return t("服务器代码未变动" + "./" + name + ".js")
     }
-
+     console.log("内容不相同")
     files.remove("./" + localname + ".js")
+  }else{
+    console.log("不存在",localname)
   }
+   console.log("写入数据",localname)
   files.ensureDir("./" + localname + ".js")
   files.write("./" + localname + ".js", content);
   t("更新成功" + "./" + name + ".js")
@@ -58,5 +65,5 @@ r("travel/ad")
 r("travel/launch")
 r("travel/gold")
 r("travel/offline")
-
+r("travel/city")
 //r("test/merge")
