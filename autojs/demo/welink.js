e9fc94c18c47ad498b517a712eb33228769d3cf3
robert88 <841354162@qq.com>
2020-03-16 10:32:20 +0800

    Update welink.js

--- autojs/demo/welink.js
+++ autojs/demo/welink.js
@@ -1,6 +1,8 @@
 //welink自动打卡
 console.show()
 
+var launchApp =  requrie("./lib/launch.js")
+
 //1
 console.log("启动app")
 launchApp("com.huawei.works")
@@ -98,25 +100,4 @@ function checkWorkEnd() {
 
 }
 
-//启动服务
-function launchApp(name) {
-  if (currentPackage() == name) {
-    console.log("服务已启动")
-    return
-  }
-  app.launch(name);
-  sleep(3000);
-  if (currentPackage() != name) {
-    throw Error("服务启动慢或者未启动")
-  } else {
-    console.log("启动完毕")
-    var skipbtn = id("tt_splash_skip_btn").findOne(2000);
-    if (skipbtn) {
-      console.log("点击跳过广告")
-      skipbtn.click();
-      sleep(5000);
-    } else {
-      console.log("启动没有发现广告")
-    }
-  }
-}
+
