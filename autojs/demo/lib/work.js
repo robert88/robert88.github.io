60842f1da5032ec8e40a933f9712e75e278ce19e
robert88 <841354162@qq.com>
2020-03-16 14:39:56 +0800

    Update work.js

--- autojs/demo/lib/work.js
+++ autojs/demo/lib/work.js
@@ -1,8 +1,9 @@
+var toArray = require("./toArray");
 var g_handler = [];
 var timer;
 var running = null;
 var g_overFlag = false;
-app.e = events.emitter();
+app.e = new events.emitter();
 
 //停止执行
 app.e.on("gameKill", function(msg) {
@@ -10,73 +11,79 @@ app.e.on("gameKill", function(msg) {
 });
 
 //添加到队列中
-app.g = function(handler, args, timeout, condition,context) {
+app.g = function(handler, args, timeout, condition, context) {
+ var handlerObj= app.createG(handler, args, timeout, condition, context);
+  g_handler.push(handlerObj);
+}
+
+//添加到队列中
+app.createG = function(handler, args, timeout, condition, context) {
   var time = new Date();
-   args = toArray(args)
+  args = toArray(args)
   timeout = time.getTime() + (timeout || 0);
   var handlerObj = {
     args: args,
-    condition:condition
+    condition: condition,
     handler: handler,
     timeout: timeout,
-    context: context,
-    status:"wait"
+    context: context
   }
-  g_handler.push(handlerObj);
   return handlerObj;
 }
 
 //释放队列
-function releaseStack(s,h){
+function releaseStack(s) {
   var time = new Date().getTime();
-  for(var i=0;i<s.length;i++){
+  for (var i = 0; i < s.length; i++) {
     var h = s[i];
-    if (time > h.timeout && h.condition()&&running==null) {
-        s.split(i,1);
-        running = h;
-        running.status="start";
-        break;
+    if (time > h.timeout && h.condition() && running == null) {
+      s.splice(i, 1);
+      running = h;
+      break;
     }
   }
 }
+
 //释放变量
-function releaseVar(){
-    g_handler = [];
-    running = null;
-    g_overFlag = false;
-    clearTimeout(timer);
+function releaseVar() {
+  g_handler = [];
+  running = null;
 }
 
 //循环执行
 function loop() {
 
   try {
-    if ( g_overFlag ) {
+    if (g_overFlag) {
       console.log("loop stop")
-      releaseVar()
+      releaseVar();
+      clearTimeout(timer);
       return;
     }
 
-  releaseStack();
-  
-  if(running&&running.status=="start"){
-    running.status="running";
-    running.handler.apply(running,running.args);
-    //需要等待执行
-    if(running.waitFor){
-      running = running.waitFor;
-    }else{
-    //执行完毕
+    releaseStack(g_handler);
+
+    if (running ) {
+
+      running.handler.apply(running, running.args);
+      
+      //需要等待执行
+      if (running.waitFor) {
+        g_handler.unshift(running.waitFor);
+
+      }
+
+      //执行完毕
       running = null;
     }
-  }
-  
-  } catch (e) {
-   e = e || {}
-   running = null
+
+  } catch (error) {
+    error = error || {}
+    releaseVar()
+    app.e.emit("systemError", error);
   }
 
- timer = setTimeout(function() {
+  timer = setTimeout(function() {
     loop();
   }, 34);
 
