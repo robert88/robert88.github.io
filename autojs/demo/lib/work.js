728289dd1e0f464b2a4622d1adaeaadf9be5eab9
robert88 <841354162@qq.com>
2020-03-19 16:32:01 +0800

    Update work.js

--- autojs/demo/lib/work.js
+++ autojs/demo/lib/work.js
@@ -1,3 +1,48 @@
+//调试
+if(global&&!global.app){
+  var currpackage;
+  global.app = {
+    launch: function(appname) {
+      console.log("call app.launch");
+      currpackage = appname;
+    }
+  }
+  var findOne =  function() {
+    return {
+      click: function() {},
+      bounds:function(){return {top:100,height:100,left:100,bottom:100,right:100,width:100}},
+      parent: function() { return { click: function() {} } }
+    }
+  }
+  
+  global.currentPackage = function() { return currpackage }
+  global.id = function(name) {
+    return {
+      findOne,
+      text(){return {findOne}},
+      textContains(){return {findOne:function(){return false}}},
+    }
+  }
+  global.className = global.id
+  global.swipe = function(){console.log("call swipe")}
+  global.click = function(){console.log("call click")}
+  
+  console.show = function() {}
+  
+  global.sleep = function() {}
+  var events = { emitter: function() { this.stack = {}; } }
+  events.emitter.prototype.on = function(e, handler) {
+    this.stack[e] = handler;
+  }
+  events.emitter.prototype.emit = function(e, ...args) {
+    console.log("call events", e);
+    if (typeof this.stack[e] == "function") {
+      this.stack[e].apply(this, args);
+    }
+  }
+}
+
+
 var toArray = require("./toArray");
 var g_handler = [];
 var timer;
@@ -11,22 +56,24 @@ app.e.on("gameKill", function(msg) {
 });
 
 //添加到队列中
-app.g = function(handler, args, timeout, condition, context) {
- var handlerObj= app.createG(handler, args, timeout, condition, context);
+app.g = function(handler, args, timeout, condition, name) {
+  var handlerObj = app.createG(handler, args, timeout, condition, name);
   g_handler.push(handlerObj);
+  console.log("添加任务<<=", name);
 }
 
 //添加到队列中
-app.createG = function(handler, args, timeout, condition, context) {
+app.createG = function(handler, args, timeout, condition, name) {
   var time = new Date();
   args = toArray(args)
   timeout = time.getTime() + (timeout || 0);
+
   var handlerObj = {
     args: args,
     condition: condition,
     handler: handler,
     timeout: timeout,
-    context: context
+    name: name
   }
   return handlerObj;
 }
@@ -36,12 +83,16 @@ function releaseStack(s) {
   var time = new Date().getTime();
   for (var i = 0; i < s.length; i++) {
     var h = s[i];
-    if (time > h.timeout && h.condition() && running == null) {
+    var condition = h.condition();
+    if (time > h.timeout && condition && running == null) {
       s.splice(i, 1);
       running = h;
+      running.conditionResult = condition;
+      console.info("执行任务=>>", running.name);
       break;
     }
   }
+  h = null;
 }
 
 //释放变量
@@ -63,10 +114,10 @@ function loop() {
 
     releaseStack(g_handler);
 
-    if (running ) {
+    if (running) {
 
-      running.handler.apply(running, running.args);
-      
+      running.handler( running.args, running.conditionResult);
+
       //需要等待执行
       if (running.waitFor) {
         g_handler.unshift(running.waitFor);
@@ -78,7 +129,8 @@ function loop() {
     }
 
   } catch (error) {
-    error = error || {}
+    error = error || {};
+    console.error("任务", running && running.name, "发生错误",error.stack);
     releaseVar()
     app.e.emit("systemError", error);
   }
@@ -90,3 +142,23 @@ function loop() {
 }
 
 loop();
+
+// function a1(a,b){
+//   console.log("a1",a,b)
+// }
+// function a2(){
+//   console.log("a2")
+// }
+// function a3(){
+//   console.log("a3")
+//   throw Error("高兴")
+// }
+// function a4(){
+//   console.log("a4")
+
+// }
+
+// app.g(a1,[1,2],0,function(){return true},"a1handler")
+// app.g(a2,[1,2],0,function(){return true},"a2handler")
+// app.g(a3,[1,2],0,function(){return true},"a3handler")
+// app.g(a4,[1,2],0,function(){return true},"a4handler")
