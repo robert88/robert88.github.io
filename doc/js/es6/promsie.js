8968bfcc74965f41ac8c789f81bbd0bba584236b
robert88 <841354162@qq.com>
2020-03-23 12:05:53 +0800

    Update promsie.js

--- doc/js/es6/promsie.js
+++ doc/js/es6/promsie.js
@@ -1,1 +1,32 @@
 
+/*
+  1、论证promise和callback性能问题
+*/
+var manyPromise=[];
+var len =100000;
+var count=100000
+var oldTime= +new Date();
+for(var i=0;i<len;i++){
+  manyPromise.push(new Promise((resove)=>{setTimeout(()=>{resove();count--},0)}))
+}
+Promise.all(manyPromise).then(()=>{
+console.log(new Date()-oldTime)
+})
+//1435ms
+
+//callback
+var len = 100000
+var count=len
+var oldTime= +new Date();
+function loop(){
+  if(count<0){
+    console.log(new Date()-oldTime)
+  }
+  count--;
+}
+for(var i=0;i<len;i++){
+  setTimeout(()=>{loop()},0)
+}
+//205839ms
+
+//看来还是promise快一些
