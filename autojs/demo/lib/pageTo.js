5333aef540e46977576a872df75defa1a5d62d2b
robert88 <841354162@qq.com>
2020-03-25 09:31:44 +0800

    Add files via upload

--- autojs/demo/lib/pageTo.js
+++ autojs/demo/lib/pageTo.js
@@ -1,8 +1,8 @@
 function pageTo(args,condition){
     console.log("点击进入页面");
     if(!condition.clickable()){
-        if(bd.parent().clickable()){
-            bd.parent().click()
+        if(condition.parent().clickable()){
+            condition.parent().click()
         }else{
             var bd = condition.bounds();
             click((bd.left+bd.right)/2,(bd.top+bd.bottom)/2);
