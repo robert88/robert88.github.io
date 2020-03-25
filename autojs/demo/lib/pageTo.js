4aba6af4550b836f2b5bcc43b4cdb1103b6fc585
robert88 <841354162@qq.com>
2020-03-25 10:04:23 +0800

    Update pageTo.js

--- autojs/demo/lib/pageTo.js
+++ autojs/demo/lib/pageTo.js
@@ -1,13 +1,17 @@
 function pageTo(args,condition){
     console.log("点击进入页面");
     if(!condition.clickable()){
+         console.log("当前条件不可点击");
         if(!args[0]&&condition.parent().clickable()){
+            console.log("当前条件父类可点击");
             condition.parent().click()
         }else{
+           console.log("点击中间点");
             var bd = condition.bounds();
             click((bd.left+bd.right)/2,(bd.top+bd.bottom)/2);
         }
     }else{
+       console.log("当前条件可点击");
       condition.click();
     }
     sleep(2000);
