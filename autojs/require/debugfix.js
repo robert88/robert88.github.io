69398e36bf340b6c103690e2a6f26034eab50de0
robert88 <841354162@qq.com>
2020-11-23 15:48:57 +0800

    Add files via upload

--- autojs/require/debugfix.js
+++ autojs/require/debugfix.js
@@ -1,43 +1,45 @@
 var wake = require("./filesystem.js")
+var restful = require("./restful.js")
 var pt = require("path")
 var currpackage;
-  global.app = {
-    launch: function(appname) {
-      console.log("call app.launch");
-      currpackage = appname;
-    }
+global.app = {
+  launch: function(appname) {
+    console.log("call app.launch");
+    currpackage = appname;
   }
-  var findOne =  function() {
-    return {
-      click: function() {},
-      bounds:function(){return {top:100,height:100,left:100,bottom:100,right:100,width:100}},
-      clickable:function(){return true},
-      parent: function() { return { click: function() {} } }
-    }
+}
+var findOne = function() {
+  return {
+    click: function() {},
+    bounds: function() { return { top: 100, height: 100, left: 100, bottom: 100, right: 100, width: 100 } },
+    clickable: function() { return true },
+    parent: function() { return { click: function() {} } }
   }
-  
-  global.currentPackage = function() { return currpackage }
-  global.id = function(name) {
-    var idObj = {
-      findOne:findOne,
-      text(){return {findOne:findOne}},
-      textContains(){return {findOne:function(){return false}}},
-    }
-    idObj.className = function(){return Object.assign({},idObj )} 
-    return idObj
-  }
-  global.className =global.id 
+}
 
-  global.swipe = function(){console.log("call swipe")}
-  global.click = function(){console.log("call click")}
-  
-  console.show = function() {}
-  console.setSize = function() {}
-  console.setPosition  = function() {}
-  
-  global.sleep = function() {}
-  global.events = { emitter: function() {
-     var stack = {};
+global.currentPackage = function() { return currpackage }
+global.id = function(name) {
+  var idObj = {
+    findOne: findOne,
+    text() { return { findOne: findOne } },
+    textContains() { return { findOne: function() { return false } } },
+  }
+  idObj.className = function() { return Object.assign({}, idObj) }
+  return idObj
+}
+global.className = global.id
+
+global.swipe = function() { console.log("call swipe") }
+global.click = function() { console.log("call click") }
+
+console.show = function() {}
+console.setSize = function() {}
+console.setPosition = function() {}
+
+global.sleep = function() {}
+global.events = {
+  emitter: function() {
+    var stack = {};
     return {
       on: function(e, handler) {
         stack[e] = handler;
@@ -49,102 +51,118 @@ var currpackage;
         }
       }
     }
-  } }
+  }
+}
 
-  global.org={autojs:{autojs:{R:{drawable:{autojs_material:{}}}}}};
-  //  android.app.Notification.Builder
-  global.android={app:{
-    NotificationChannel:function(){
-      this.enableLights = function(){}
-      this.setLightColor = function(){}
-      this.setShowBadge = function(){}
+global.org = { autojs: { autojs: { R: { drawable: { autojs_material: {} } } } } };
+//  android.app.Notification.Builder
+global.android = {
+  app: {
+    NotificationChannel: function() {
+      this.enableLights = function() {}
+      this.setLightColor = function() {}
+      this.setShowBadge = function() {}
     },
-    Service:{NOTIFICATION_SERVICE:1},
-    NotificationManager:{
-      IMPORTANCE_DEFAULT:1
+    Service: { NOTIFICATION_SERVICE: 1 },
+    NotificationManager: {
+      IMPORTANCE_DEFAULT: 1
     },
-    Notification:{
-      Builder:function(){
+    Notification: {
+      Builder: function() {
 
-    this.setContentTitle=function(){return this;}
-    this.setContentText=function(){return this;}
-    this.setWhen=function(){return this;}
-    this.setSmallIcon=function(){return this;}
-    this.setTicker=function(){return this;}
-    this.build=function(){return this;}
-    return this;
-  }}}};
-  
-  global.context={
-    getSystemService:function(ret){
-      if(ret==android.app.Service.NOTIFICATION_SERVICE){
-        return {
-          createNotificationChannel:function(){},
-          notify:function(){}
+        this.setContentTitle = function() { return this; }
+        this.setContentText = function() { return this; }
+        this.setWhen = function() { return this; }
+        this.setSmallIcon = function() { return this; }
+        this.setTicker = function() { return this; }
+        this.build = function() { return this; }
+        return this;
+      }
+    }
+  }
+};
+
+global.context = {
+  getSystemService: function(ret) {
+    if (ret == android.app.Service.NOTIFICATION_SERVICE) {
+      return {
+        createNotificationChannel: function() {},
+        notify: function() {}
+      }
+    }
+    return {}
+  }
+}
+
+global.device = {
+  sdkInt: 58,
+  width: 720,
+  height: 1920
+}
+
+global.storages = { getItem: function() {}, setItem: function() {}, clear: function() {} }
+
+function getCallPath() {
+  try {
+    throw Error("path error stack look for call path")
+  } catch (e) {
+    var stack = e.stack;
+    stack = stack.split(/\n/)
+  }
+  var path = /\((.*):\d*:\d*\)/.exec(stack[3])
+  return path && path[1];
+}
+global.files = {
+  ensureDir: function() {},
+  write: function(path, content) {
+    curpath = pt.dirname(getCallPath())
+    if (curpath) {
+      path = pt.resolve(curpath, path);
+    } else {
+      path = pt.resolve(curpath, path);
+    }
+
+    wake.writeData(path, content)
+  },
+  exists: function(path) {
+    curpath = pt.dirname(getCallPath())
+    if (curpath) {
+      path = pt.resolve(curpath, path);
+    } else {
+      path = pt.resolve(curpath, path);
+    }
+    return wake.isExist(path)
+  }
+}
+var rootPath = __dirname
+global.http = {
+  get(url, params, callback) {
+    var filename = pt.basename(url);
+    var ret
+    try{
+       ret = wake.readData(rootPath + "/res/" + filename + ".html")
+    }catch(e){
+       ret = wake.readData(rootPath + "/res/" + "demo" + ".html")
+    }
+
+    callback({
+      statusCode:200,
+      body: {
+        string() {
+          return ret;
         }
       }
-      return {}
-    }
+    })
+    // restful({url:url,data:params,function (params) {
+    //   callback({
+      // statusCode:200,
+    //     body:{string(){
+    //       return ret;
+    //     }}})
+    // }}) 
   }
+}
 
-  global.device = {
-    sdkInt:58,
-    width:720,
-    height:1920
-  }
-
-  global.storages = {getItem:function(){},setItem:function(){},clear:function(){}}
-
-  function getCallPath(){
-    try{
-      throw Error("path error stack look for call path")
-    }catch(e){
-      var stack =e.stack;
-     stack =  stack.split(/\n/)
-    }
-    var path = /\((.*):\d*:\d*\)/.exec(stack[3])
-    return path&&path[1];
-  }
-  global.files = {
-    ensureDir:function(){},
-    write:function(path,content){
-      curpath = pt.dirname(getCallPath())
-      if(curpath){
-        path = pt.resolve(curpath,path);
-      }else{
-        path = pt.resolve(curpath,path);
-      }
-
-      wake.writeData(path,content)
-    },
-    exists:function(path){
-      curpath = pt.dirname(getCallPath())
-      if(curpath){
-        path = pt.resolve(curpath,path);
-      }else{
-        path = pt.resolve(curpath,path);
-      }
-      return wake.isExist(path)
-    }
-  }
-  var rootPath = __dirname.replace("lib","")
-  global.http={
-    get(url){
-      var ret;
-      if(url=="https://github.com/robert88/robert88.github.io/file-list/master/autojs/demo"){
-        ret = wake.readData(rootPath+"/res/demo.html")
-      }else{
-        ret = wake.readData(rootPath+"/res/lib.html")
-      }
-
-      return {
-        body:{string(){
-          return ret;
-        }}
-      } 
-    }
-  }
-
-  global.toast = function(msg){
-    console.log("toast",msg)
-  }
+global.toast = function(msg) {
+  console.log("toast", msg)
+}
--- autojs/require/history.js
+++ autojs/require/history.js
@@ -3,19 +3,20 @@ var parse = require("./parse.js");
 
 
 //请求页面数据
-function loadPageHtml(url) {
-  try {
-    console.log("请求url", url)
-    var res = http.get(url, {});
-    sleep(5000);
-    return res.body.string();
-  } catch (e) {
-    console.log("请求失败", e.message, url, e.stack)
-  }
-  return "";
+function loadPageHtml(url, callback) {
+  console.log("异步请求url", url)
+  http.get(url, {}, res => {
+    if (res.statusCode == 200) {
+      callback(res.body.string());
+    } else {
+      console.log("error:", res.statusCode + ":" + "ajax ERROR")
+    }
+  });
 }
 
-
+/*
+parse html结构
+*/
 function find(content, tag, className, attrsName) {
   if (content) {
     var li = parse(tag, content.innerHTML)
@@ -31,34 +32,51 @@ function find(content, tag, className, attrsName) {
   }
   return ""
 }
+/*
+  得到文件数
+*/
+function parseTreeData(githubPage, obj, callback) {
 
-function parseTreeData(githubPage, obj) {
-  var html = loadPageHtml(githubPage);
-  var parentDir = parentDir || "";
   var obj = obj || {
     dirs: {},
-    files: {}
+    files: {},
+    time: 0
   };
 
-  //得到内容
-  var content = find({
-    innerHTML: html
-  }, "div", "row", "role");
-  content.forEach(item => {
-    if (~item.attrs["class"].indexOf("py-2")) {
-      item = find(item, "div");
-      var isDir = ~item[0].template.indexOf('aria-label="Directory"');
-      var filename = find(item[1], "a")[0].innerHTML
-      var time = new Date(find(item[3], "time-ago")[0].attrs.datetime).getTime()
-      if (isDir) {
-        obj.dirs[githubPage + "/" + filename] = time;
-        parseTreeData(githubPage + "/" + filename, obj)
-      } else {
-        obj.files[githubPage + "/" + filename] = time;
+  loadPageHtml(githubPage, html => {
+    //得到内容
+    var content = find({
+      innerHTML: html
+    }, "div", "row", "role");
+
+    obj.time += content.length;
+
+    content.forEach(item => {
+      obj.time--;
+      if (~item.attrs["class"].indexOf("py-2")) {
+        item = find(item, "div");
+        var isDir = ~item[0].innerHTML.indexOf('aria-label="Directory"');
+        var filename = find(item[1], "a")[0].innerHTML.trim()
+        var time = new Date(find(item[3], "time-ago")[0].attrs.datetime).getTime()
+        if (isDir && filename && !obj.dirs[githubPage + "/" + filename]) {
+          obj.dirs[githubPage + "/" + filename] = time;
+          parseTreeData(githubPage + "/" + filename, obj)
+        } else if(~filename.indexOf(".")){
+          obj.files[githubPage + "/" + filename] = time;
+        }
       }
+    })
+
+    if (obj.time == 0) {
+      callback(obj);
     }
-  })
-  return obj;
+
+
+  });
+
+
+
+
 }
 
 //写文件
@@ -81,24 +99,32 @@ function writeTree(treedata, cacheInfo, githubPage, githubLoad) {
 
 /**ajax请求 */
 function r(loadFile, localFile) {
-  var content 
 
-  try {
-    console.log("请求：", loadFile, " 即将写入文件：", localFile)
-    var res = http.get(loadFile, {});
-    sleep(5000)
-     content = res.body.string();
-  } catch (e) {
-    console.log(e)
-    console.log(e.stack);
-
-  }
-  try {
-    w(localFile,  content )
-  } catch (e) {
-    t("更新失败" + localFile);
+  if (global.debugger) {
+    localFile = ("build/" + localFile).replace(/\/+/g, "/");
+  } else {
+    localFile = "./demo/" + (localFile).replace(/\/+/g, "/");
   }
 
+  console.log("异步请求：", loadFile,"即将写入文件：",localFile);
+  
+  http.get(loadFile, {}, res => {
+    if (res.statusCode == 200) {
+      w(localFile, res.body.string());
+    } else {
+      console.log("资源文件请求失败", e.message, e.stack);
+    }
+  });
+}
+
+
+/*写文件*/
+function w(localFile, content) {
+  console.log("写入数据到文件：", localFile)
+  sleep(1000)
+  files.ensureDir(localFile)
+  files.write(localFile, content);
+  t( localFile+" 写入成功")
 }
 
 /* 消息队列*/
@@ -107,31 +133,23 @@ function t(msg) {
   sleep(1000)
 }
 
-/*写文件*/
-function w(localFile,  content ) {
-  if (global.debugger) {
-    localFile = ("build/" + localFile).replace(/\/+/g, "/");
-  } else {
-    localFile = "./"+(localFile).replace(/\/+/g, "/");
-  }
-  console.log("写入数据到文件：", localFile)
-  sleep(2000)
-  files.ensureDir(localFile)
-  files.write(localFile, content);
-  t("更新成功" + localFile)
-}
+//请求地址
 
-module.exports = function (githubPage, githubLoad) {
+module.exports = function(githubPage, githubLoad) {
 
   var cacheInfo = {}
   if (files.exists("./cache.js")) {
-    cacheInfo = require("../cache.js")
+    cacheInfo = require("../cache.js"); // require的地址不一样
   }
+
   //解析github文件列表
-  var treedata = parseTreeData(githubPage);
+  parseTreeData(githubPage, null, treedata => {
 
-  writeTree(treedata, cacheInfo, githubPage, githubLoad)
+    writeTree(treedata, cacheInfo, githubPage, githubLoad);
+    files.write("./cache.js", "module.exports=" + JSON.stringify(treedata));
 
-  files.write("./cache.js", "module.exports=" + JSON.stringify(treedata))
+  });
+
+
 
 }
