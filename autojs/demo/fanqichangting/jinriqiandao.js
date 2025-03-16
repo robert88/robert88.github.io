3b104bb3e29d705bb05a827b7e9dd1f2d716b60e
fubao <15580564+rapserver@user.noreply.gitee.com>
2025-03-16 08:54:41 +0800

    今日领取弹框

--- autojs/demo/fanqichangting/jinriqiandao.js
+++ autojs/demo/fanqichangting/jinriqiandao.js
@@ -15,14 +15,32 @@ exports.checkIsJinriqiandaoDialog = function () {
     const keyWorkDay = className(textClassName).descContains("第").find()
     const [left, top, right, bottom] = [323, 1146, 397, 1220]
     const closeButtom = className(dialogUiClassName).boundsContains(left + 10, top + 10, right - 10, bottom - 10).find()
-    l("弹框" + dialogUi.size())
-    l("点击领取按钮" + getMoneyButtom.size())
-    l("点击领取按钮" + tomorrowButtom.size())
-    l("头部小标题" + topSubTitle.size())
+    const dsize = dialogUi.size()
+    const msize = getMoneyButtom.size()
+    const tmsize = tomorrowButtom.size()
+    const tstsize = topSubTitle.size()
+    const ttsize = topTips.size()
+    const ksize = keyWorkDay.size()
+    l("弹框" + dsize)
+    l("点击领取按钮" + msize)
+    l("明日再来" + tmsize.size())
+    l("头部小标题" + tstsize)
     l("领取按钮下的说明" + tips.size())
-    l("领取左上角标题" + topTips.size())
-    l("关键字 ‘第’" + keyWorkDay.size())
-    l("关闭按钮" + keyWorkDay.size())
-    l("确认为今日签到弹框")
+    l("领取左上角标题" + ttsize)
+    l("关键字 ‘第’" + ksize)
+    l("关闭按钮" + closeButtom.size())
+
+    if (dsize && (msize || tmsize) && tstsize && ksize) {
+        l("确认为今日签到弹框")
+        if (msize) {
+            return getMoneyButtom
+        } else {
+            return tomorrowButtom
+        }
+    } else {
+        l(l("当前不是今日签到弹框"))
+        return null
+    }
+
 }
 
