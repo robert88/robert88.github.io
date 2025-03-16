abcef123ad45b8f0ae9eeff0c313cf98feb02153
fubao <15580564+rapserver@user.noreply.gitee.com>
2025-03-16 09:10:41 +0800

    今日领取弹框

--- autojs/demo/fanqichangting/jinriqiandao.js
+++ autojs/demo/fanqichangting/jinriqiandao.js
@@ -3,24 +3,28 @@
  */
 
 //检查今日签到弹框
-exports.checkIsJinriqiandaoDialog = function () {
+exports.checkIsJinriqiandaoDialog = function ($ui, $text) {
     const dialogUiClassName = "com.lynx.tasm.behavior.ui.LynxFlattenUI"
     const textClassName = "com.lynx.tasm.behavior.ui.text.FlattenUIText"
-    const dialogUi = className(dialogUiClassName).descContains("今日签到").find()
-    const getMoneyButtom = className(dialogUiClassName).text("领取今日现金").find()
-    const tomorrowButtom = className(dialogUiClassName).text("明日再来").find()
-    const topSubTitle = className(textClassName).text("今日签到必得现金红包").find()
-    const tips = className(textClassName).text("待领取为最高值，收益以实发为准").find()
-    const topTips = className(textClassName).text("现金可微信提现").find()
-    const keyWorkDay = className(textClassName).descContains("第").find()
+    $ui = $ui || className(dialogUiClassName)
+    $text = $text || className(textClassName)
+    const dialogUi = ui.descContains("今日签到").find()
+    const getMoneyButtom = $ui.text("领取今日现金").find()
+    const tomorrowButtom = $ui.text("明日再来").find()
+    const topSubTitle = $text.text("今日签到必得现金红包").find()
+    const tips = $text.text("待领取为最高值，收益以实发为准").find()
+    const topTips = $text.text("现金可微信提现").find()
+    const keyWorkDay = $text.descContains("第").find()
     const [left, top, right, bottom] = [323, 1146, 397, 1220]
-    const closeButtom = className(dialogUiClassName).boundsContains(left + 10, top + 10, right - 10, bottom - 10).find()
+    const closeButtom = $ui.boundsContains(left + 10, top + 10, right - 10, bottom - 10).find()
     const dsize = dialogUi.size()
     const msize = getMoneyButtom.size()
     const tmsize = tomorrowButtom.size()
     const tstsize = topSubTitle.size()
     const ttsize = topTips.size()
     const ksize = keyWorkDay.size()
+    l("弹框" + $ui.size())
+    l("弹框" + $text.size())
     l("弹框" + dsize)
     l("点击领取按钮" + msize)
     l("明日再来" + tmsize.size())
